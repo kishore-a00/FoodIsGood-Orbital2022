54c8f45847b249abad96f1f2e4ea489dc309cd57
Jordan Kwua <kwuachunren@gmail.com>
2022-06-23 21:03:33 +0800

    Implemented upvoting function and sorting based on number of upvotes fixes #7

--- screens/ItemScreen.js
+++ screens/ItemScreen.js
@@ -1,37 +1,62 @@
 import React, { useEffect, useState } from "react";
-import { Text, View, FlatList, StyleSheet, Image, TouchableOpacity, Alert } from "react-native";
+import {
+  Text,
+  View,
+  FlatList,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Alert,
+  Button,
+  Touchable,
+} from "react-native";
 import { supabase } from "../lib/supabase";
 
 //Functionality to fetch image from the database
 //{item_id: route.params.item_id} in review section. Be able to pass the data to the ReviewScreen
 //Unable to add image inside
 
-export const ItemScreen = ( {navigation, route} ) => {
+export const ItemScreen = ({ navigation, route }) => {
   const [review, setReview] = useState("");
+  const [pressed, setPressed] = useState(false);
+  var value_to_update = 0;
+  var update_id = 0;
 
   //Filtering the database based on the item the user selected in StallScreen
   const ShowReview = async () => {
     let { data: all_reviews, error } = await supabase
       .from("all_reviews")
       .select("*")
+      .order("votes", { ascending: false })
       .eq("item_id", route.params.item_id);
     return { all_reviews, error };
   };
   const LoadReview = async () => {
     const { all_reviews, error } = await ShowReview();
     setReview(all_reviews);
-    // console.log(all_reviews);
-    // console.log(error);
   };
   useEffect(() => {
     LoadReview();
   });
+  // Function updates the database with the review's new votecount
+  const updateVote = async () => {
+    setPressed(true);
+    const { data, error } = await supabase
+      .from("all_reviews")
+      .update({ votes: value_to_update })
+      .eq("review_id", update_id);
+  };
 
   return (
     <View>
       {/* Post a review button */}
       <Text style={styles.headertext}> {route.params.item_name} </Text>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Review", {item_id: route.params.item_id})}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() =>
+          navigation.navigate("Review", { item_id: route.params.item_id })
+        }
+      >
         <Text style={styles.buttontext}> Post a review! </Text>
       </TouchableOpacity>
 
@@ -46,18 +71,34 @@ export const ItemScreen = ( {navigation, route} ) => {
 
       {/* Header for reviews section */}
       <Text style={styles.subheadertext}>Reviews:</Text>
-      
+
       {/* Listing of reviews and ratings */}
       <FlatList
         contentContainerStyle={{ paddingBottom: 200 }}
         keyExtractor={(item) => item.review_id}
         data={review}
         renderItem={({ item }) => (
-            <Text style={styles.reviewtext}> 
-              Review: {item.review} {'\n'} 
-              Taste: {item.taste} {'\n'}
-              Value for money: {item.money} 
+          <View>
+            <TouchableOpacity
+              style={styles.upvotebutton}
+              onPress={() => {
+                value_to_update = item.votes + 1;
+                update_id = item.review_id;
+                updateVote();
+              }}
+              disabled={pressed}
+            >
+              <Text style={{ color: "white" }}>Vote!</Text>
+              <Text style={{ textAlign: "center", color: "white" }}>
+                {item.votes}
+              </Text>
+            </TouchableOpacity>
+            <Text style={styles.reviewtext}>
+              Review: {item.review} {"\n"}
+              Taste: {item.taste} {"\n"}
+              Value for money: {item.money}
             </Text>
+          </View>
         )}
       />
     </View>
@@ -71,7 +112,7 @@ const styles = StyleSheet.create({
   headertext: {
     color: "#A7BC5B",
     fontSize: 28,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   button: {
     alignItems: "center",
@@ -82,13 +123,13 @@ const styles = StyleSheet.create({
   },
   buttontext: {
     fontSize: 20,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   subheadertext: {
     fontSize: 25,
     paddingHorizontal: 10,
-    fontWeight: 'bold',
-    color: "#A7BC5B"
+    fontWeight: "bold",
+    color: "#A7BC5B",
   },
   image: {
     alignSelf: "center",
@@ -99,5 +140,12 @@ const styles = StyleSheet.create({
     padding: 15,
     marginVertical: 10,
     marginHorizontal: 10,
-  }
+  },
+  upvotebutton: {
+    alignSelf: "flex-end",
+    backgroundColor: "#ff4500",
+    padding: 5,
+    marginVertical: 10,
+    marginHorizontal: 10,
+  },
 });
