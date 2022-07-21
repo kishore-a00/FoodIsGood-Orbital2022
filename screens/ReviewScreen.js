6839eb3ff5838e00d1963d881c22011d04edbd9b
Kishore <skippyepic@gmail.com>
2022-07-21 21:08:49 +0800

    Updated rating and review screen, fixes #20
    Added info button to help users understand what to do on the review page. Added word description beside the numeric ratings for ease of understanding of what the numeric ratings imply. Changed up UI to make the screen look nicer.

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,18 +1,38 @@
 import Slider from "@react-native-community/slider";
 import React, { useState } from "react";
-import { Text, View, TouchableOpacity, StyleSheet, TextInput, Button, Keyboard, Alert } from "react-native";
+import { Text, View, TouchableOpacity, StyleSheet, TextInput, Button, Keyboard, Alert, TouchableWithoutFeedback, Modal } from "react-native";
 import { supabase } from "../lib/supabase";
 
 //Try keyboard avoiding view
-//Click out to hide keyboard
 //data: all_reviews?
 //Implementing RatingSystem component instead of importing the relevant components
 
 export default function ReviewScreen({ navigation, route }) {
-    const [taste, setTaste] = useState();
-    const [money, setMoney] = useState();
+    //Storing the rating value for taste
+    const [taste, setTaste] = useState(2); //Setting the numeric value for taste
+    const [taste_desc, setTaste_desc] = useState("okay"); //Setting the description for how tasty the food is
+    const arrayTaste = ["", "meh", "okay", "good", "very nice", "fantastic!"]; //Rating starts from 1 whereas index starts from 0
+
+    //Storing the rating value for value
+    const [money, setMoney] = useState(2); //Setting the numeric value for value
+    const [money_desc, setMoney_desc] = useState("quite ex"); //Setting the description for how value for money the food is
+    const arrayMoney = ["", "not worth", "quite ex", "good value", "great value", "fantastic value!"]; //Rating starts from 1 whereas index starts from 0
     const [review, setReview] = useState("");
-    let user_uuid = supabase.auth.user().id; //not sure
+
+    //const[modalVisible, setModalVisible] = useState(false);
+    let user_uuid = supabase.auth.user().id; 
+
+    //Creating an info button at the top right corner of the screen
+     React.useLayoutEffect(() => {
+       navigation.setOptions({
+         headerRight: () =>
+         <Button 
+         onPress={() => 
+          Alert.alert("Welcome to the Rating section!", 
+          "Place your finger on the knob of the slider and slide horizontally to your desired rating. \n\nClick the \"Type a review\" area and type out your review! \n\nSatisfied with your review? \n\nClick the \"Submit review!\" button to submit your review!", [{text: "Ok", onPress: () => console.log("pressed")}])} 
+        title="?" />,
+        })
+      })
 
     //This function will be called upon pressing the submit button.
     //It submits the review, the ratings, the user_id and the item id to the database
@@ -33,12 +53,18 @@ export default function ReviewScreen({ navigation, route }) {
         addReview()
       }
     };
-  
+
     return (
       <View style={styles.container}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
           <View>
+            <View style={{marginTop: 10, marginBottom: 20}}>
+              <Text style={styles.headerText}>Currently reviewing:</Text>
+              <Text style={styles.itemNameText}>{route.params.item_name}</Text>
+            </View>
+            <View style={styles.sliderContainer}>
               {/* First Slider for Taste */}
-              <Text style={styles.innerText}>Taste: {taste} </Text> 
+              <Text style={styles.innerText}>Taste: {taste} ({taste_desc})</Text> 
               <Slider
               style={{ width: 200, height: 50 }}
               minimumValue={1} //Minimum value of rating
@@ -47,12 +73,14 @@ export default function ReviewScreen({ navigation, route }) {
               maximumTrackTintColor="#000000"
               step={1} //Increment slider by one step
               value={2} //Starting value of slider is 2
-              onValueChange={(value) => setTaste(value)} //not sure if needed
-              thumbTintColor="green"
+              onValueChange={(value) => { setTaste(value), setTaste_desc(arrayTaste[value]) }} //not sure if needed
+              thumbTintColor="#ade8f4"
               />
+            </View>
   
               {/* Second Slider for Value for money */}
-              <Text style={styles.innerText}>Value for money: {money} </Text>
+            <View style={styles.sliderContainer}>  
+              <Text style={styles.innerText}>Value for money: {money} ({money_desc}) </Text>
               <Slider
               style={{ width: 200, height: 50 }}
               minimumValue={1} //Minimum value of rating
@@ -61,29 +89,28 @@ export default function ReviewScreen({ navigation, route }) {
               maximumTrackTintColor="#000000"
               step={1} //Increment slider by one step
               value={2} //Starting value of slider is 2
-              onValueChange={(value) => setMoney(value)} //not sure if needed
-              thumbTintColor="green"
+              onValueChange={(value) => { setMoney(value), setMoney_desc(arrayMoney[value]) }} //not sure if needed
+              thumbTintColor="#ade8f4"
               />
+            </View>
   
               {/* Section to write review, REMOVED TEXT INPUT REF*/}
+              <View>
               <TextInput
               multiline={true}
-              numberOfLines={4}
+              numberOfLines={5}
               placeholder="Type Review Here"
               value={review}
               onChangeText={(v) => setReview(v)}
-              style={{
-              marginBottom: 12,
-              height: 70,
-              justifyContent: "flex-start",
-              }}
-              
-          />
+              style={styles.input}
+              />
+              </View>
           </View>
+        </TouchableWithoutFeedback>
 
           {/* Submit button to submit both the rating and the review to the database */}
           <TouchableOpacity style={styles.button} onPress={() => checkSubmission()}>
-              <Text>Submit review!</Text>
+              <Text style={styles.submitText}>Submit review!</Text>
           </TouchableOpacity>
   
       </View>
@@ -93,10 +120,11 @@ export default function ReviewScreen({ navigation, route }) {
   const styles = StyleSheet.create({
     innerText: {
       fontSize: 20,
+      fontWeight: "200"
     },
     button: {
       alignItems: "center",
-      backgroundColor: "#8DA242",
+      backgroundColor: "#90e0ef",
       padding: 20,
       marginVertical: 10,
       marginHorizontal: 10,
@@ -107,5 +135,27 @@ export default function ReviewScreen({ navigation, route }) {
     },
     sliderContainer: {
       alignItems: "center",
+      paddingVertical: 0
+    },
+    input: {
+      height: 100,
+      margin: 10,
+      borderWidth: 1,
+      padding: 10,
+      marginTop: 0
+    },
+    submitText: {
+      fontSize: 20,
+      fontWeight: "700"
+    },
+    headerText: {
+      fontSize: 17,
+      fontWeight: "100",
+      textAlign: "center"
+    },
+    itemNameText: {
+      fontSize: 25,
+      fontWeight: "bold",
+      textAlign: "center"
     },
   });
